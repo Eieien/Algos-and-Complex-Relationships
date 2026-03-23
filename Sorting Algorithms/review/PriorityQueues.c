5de1491adf110d95f970383ccbb0130b744da937
Eien <depedronico02@gmail.com>
2026-03-24 01:52:05 +0800

    Heapsort

--- Sorting Algorithms/review/PriorityQueues.c
+++ Sorting Algorithms/review/PriorityQueues.c
@@ -25,7 +25,7 @@ void initPOT(HEAP* H){
     H->last = 0;
 }
 
-void heapify(HEAP* H){
+void maxHeapifyUp(HEAP* H){
     for(int i = H->last; H->arr[parentIdx(i)] < H->arr[i]; i = parentIdx(i)){
         int temp = H->arr[i];
         H->arr[i] = H->arr[parentIdx(i)];
@@ -33,14 +33,80 @@ void heapify(HEAP* H){
     }
 }
 
+void maxHeapify(HEAP* H, int i){
+
+    int largest = i;
+    int r = RightChildIdx(i);
+    int l = leftChildIdx(i);
+    int n = H->last;
+
+    if(l < n && H->arr[l] > H->arr[largest]){
+        largest = l;
+    }
+
+    if(r < n && H->arr[r] > H->arr[largest]){
+        largest = r;
+    }
+
+    if(largest != i){
+        int temp = H->arr[largest];
+        H->arr[largest] = H->arr[i];
+        H->arr[i] = temp;
+    
+        maxHeapify(H, largest);
+    }
+
+}
+
+void minHeapify(HEAP* H, int i){
+    int smallest = i;
+    int r = RightChildIdx(i);
+    int l = leftChildIdx(i);
+    int n =H->last;
+    if(l < n && H->arr[l] < H->arr[smallest]){
+        smallest = l;
+    }
+
+    if(r < n && H->arr[r] < H->arr[smallest]){
+        smallest = r;
+    }
+
+    if(smallest != i){
+        
+        int temp = H->arr[smallest];
+        H->arr[smallest] = H->arr[i];
+        H->arr[i] = temp;
+
+        minHeapify(H, smallest);
+    }
+}
+
+void deleteMax(HEAP* H){
+    int temp = H->arr[0];
+    H->arr[0] = H->arr[H->last - 1];
+    H->arr[H->last - 1] = temp;
+    // printf("%d \n", H->arr[H->last - 1]);
+    H->last--;
+    maxHeapify(H, 0);
+}
+
+void heapSort(HEAP* H){
+    int last = H->last;
+    for(int i = 0; i < last; i++){
+        deleteMax(H);
+    }
+
+    H->last = last;
+
+}
+
 void insert(HEAP* H, int val){
     
     H->arr[H->last] = val;
-    heapify(H);
+    maxHeapifyUp(H);
     
     H->last++;
 }
-
 
 void display(HEAP H){
     
@@ -56,18 +122,27 @@ int main(){
 
     HEAP H;
     initPOT(&H);
-    insert(&H, 50);
     insert(&H, 40);
     insert(&H, 30);
     insert(&H, 17);
-    insert(&H, 13);
-    insert(&H, 12);
-    insert(&H, 11);
     insert(&H, 7);
     insert(&H, 4);
+    insert(&H, 4);
+    insert(&H, 4);
+    insert(&H, 50);
     insert(&H, 6);
     insert(&H, 43);
+    
+    display(H);
+    // deleteMax(&H);
+    // deleteMax(&H);
+    // deleteMax(&H);
+    // deleteMax(&H);
+    // deleteMax(&H);
+    // deleteMax(&H);
+    // deleteMax(&H);
 
+    heapSort(&H);
     display(H);
 
     return 0;
